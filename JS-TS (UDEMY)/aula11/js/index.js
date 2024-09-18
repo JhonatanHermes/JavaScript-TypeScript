83a5df20807360b839192187a40e0d80da9c94b7
Jhonatan Lucco <149596203+JhonatanHermes@users.noreply.github.com>
2024-09-17 21:41:00 -0300

    springs

---
+++ JS-TS (UDEMY)/aula11/js/index.js
@@ -0,0 +1,12 @@
+/*alert('Mensagem de teste')
+window.confirm('Deseja apagar?')
+window.prompt('Digite seu nome:') */
+
+// let confirma = confirm('Aceita?')
+
+let num1 = prompt('Digite um número:')
+let num2 = prompt('Digite um número:')
+num1 = Number(num1)
+num2 = Number(num2)
+const resultado = num1 + num2
+alert(`O resultado foi de ${resultado}`)
---
+++ JS-TS (UDEMY)/aula13/index.js
@@ -0,0 +1,8 @@
+//               01234567
+let umaString = 'Um texto'
+
+console.log(umaString[4])
+console.log(umaString.slice(3, 8))
+console.log(umaString.length)
+console.log(umaString.toUpperCase())
+console.log(umaString.toLowerCase())

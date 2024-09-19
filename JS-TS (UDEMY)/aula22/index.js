f368a85c717b34b6d81fe28f7e429d14a6682200
Jhonatan Lucco <149596203+JhonatanHermes@users.noreply.github.com>
2024-09-19 11:13:57 -0300

    if / else if / else

---
+++ JS-TS (UDEMY)/aula22/index.js
@@ -0,0 +1,16 @@
+// const expessaoAnd = true && true
+
+/*
+const expressaoOr = true || false
+console.log(expessaoOr)
+*/
+
+/*
+const usuario = 'Jhonatan' // form
+const senha = '1234' // form
+
+const vaiLogar = usuario === 'Jhonatan' && senha === '1234'
+console.log(vaiLogar)
+*/
+
+console.log(!true)
---
+++ JS-TS (UDEMY)/aula23/index.js
@@ -0,0 +1,22 @@
+//console.log('Jhonatan Lucco' && true && 'Amanda')
+//console.log('Jhonatan Lucco' && true && 'Amanda')
+
+//console.log('Jhonatan' && '' && 'Amanda')
+//console.log('Jhonatan' && 'Amanda')
+
+/*
+function falaOi() {
+    return 'Oi'
+}
+
+let vaiExecutar = true  
+
+console.log(vaiExecutar && falaOi())
+*/
+
+/*
+const corUsuario = null
+const corPadrao = corUsuario || 'preto'
+
+console.log(corPadrao)
+*/

c35b558574b6db890abdce77a4be23d95b8d89a4
Jhonatan Lucco <149596203+JhonatanHermes@users.noreply.github.com>
2024-10-07 14:56:22 -0300

    arrays e splice

---
+++ JS-TS (UDEMY)/aula62/index.js
@@ -0,0 +1,57 @@
+/*
+const nomes = ['Amanda', 'Moreira', 'Cesario']
+const nome = nomes.join(' ')
+console.log(nome)
+*/
+
+
+/*
+const nome = 'Japão, Alemanha, Irlanda'
+const nomes = nome.split(',')
+*/
+
+/*
+const nomes = ['Diana', 'Bruce', 'Clark', 'Wally']
+const novo = nomes.slice(1, -1)
+console.log(novo)
+*/
+
+
+/*
+const nomes = ['Alan', 'Maethe', 'Remy']
+nomes.unshift('Ellie')
+nomes.unshift('Flinn')
+console.log(nomes)
+*/
+
+
+/*
+const nomes = ['Eduardo', 'João', 'Maria']
+nomes.push('Amanda')
+nomes.push('Jhonatan')
+console.log(nomes)
+*/
+
+
+/*
+const nomes = ['Amanda', 'Jhonatan', 'Aki']
+const removido = nome.shift()
+console.log(nomes, removido)
+*/
+
+/*
+const nomes = ['Amanda', 'Izobell', 'Levi']
+const novo = [...nomes]
+
+novo.pop()
+console.log(nomes)
+console.log(novo)
+*/
+
+
+/*
+const nomes = ['Eduardo', 'Maria', 'João']
+nomes[2] = 'João'
+delete nomes[2]
+console.log(nomes)
+*/
---
+++ JS-TS (UDEMY)/aula63/index.js
@@ -0,0 +1,4 @@
+const nomes = ['Amanda', 'Jhonatan', 'Izobell', 'Levi', 'Aki']
+
+const removidos = nomes.splice(4, 0, 'Sasicha')
+console.log(nomes, removidos)

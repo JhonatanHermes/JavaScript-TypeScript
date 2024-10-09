10710ceedeb6f7396002a7344ece550a0736dc1f
Jhonatan Lucco <149596203+JhonatanHermes@users.noreply.github.com>
2024-10-09 14:44:06 -0300

    Herança

---
+++ JS-TS (UDEMY)/aula76/index.js
@@ -0,0 +1,48 @@
+function Produto(nome, preco) {
+    this.nome = nome
+    this.preco = preco
+}
+
+Produto.prototype.aumento = function(qunatia) {
+    this.preco *= qunatia
+}
+
+Produto.prototype.desconto = function(qunatia) {
+    this.preco -= qunatia
+}
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco)
+    this.cor = cor
+}
+Camiseta,prototype = Object.create(Produto.prototype)
+Camiseta.prototype.constructor = Camiseta
+
+Camiseta.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100))
+}
+
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco)
+    this.material = material
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+            return estoque
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number') return
+            estoque = valor
+        }
+    })
+}
+Caneca.prototype = Object.create(Produto.prototype)
+Caneca.prototype.constructor = Caneca
+
+const camiseta = new Camiseta('regata', 7.5, 'preta')
+const caneca = new Caneca('Caneca', 15, 'Plástico', 5)
+
+console.log(camiseta)
+console.log(caneca)
